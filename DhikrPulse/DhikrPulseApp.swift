317f79a4bdb8efaecceed77fc091b69e27b15d31
Çağrı Özay <cagriozay13@gmail.com>
2026-03-17 13:58:56 +0300

    Initial Commit

---
+++ DhikrPulse/DhikrPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DhikrPulseApp.swift
+//  DhikrPulse
+//
+//  Created by Çağrı Özay on 17.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct DhikrPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
